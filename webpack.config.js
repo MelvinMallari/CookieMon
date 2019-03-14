fd900e587bf218f07d4b259d4aa6096358190b51
Melvin Mallari <mtmallari@ucdavis.edu>
2019-03-14 14:30:23 -0700

    Create Outter Top Left maze portionn

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './lib/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'bundle.js'
+  }
+};
